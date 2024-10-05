a0f917739c8ceefc728dbf44f77b8be529c978bd
Quang Akiyama <quanganhcf2@gmail.com>
2024-10-05 19:16:51 +0700

    finish carousel screen

---
+++ src/data/data.ts
@@ -0,0 +1,39 @@
+import khmer from "../assets/image/khmer.jpg";
+import nung from "../assets/image/nung.jpg";
+import khomu from "../assets/image/khomu.jpg";
+import ede from "../assets/image/ede.jpg";
+
+export const arrayImg = [
+  {
+    image: khmer,
+    title: "Khmer",
+    characteristic:
+      "Khmer women typically wear long skirts, blouses, and sarongs. The long skirts often have multiple layers and are adorned with intricate patterns. Blouses feature round or V-necklines and can have short or long sleeves. Sarongs are wrapped around the waist, creating a graceful and elegant appearance. Khmer attire is also influenced by religion, particularly Buddhism.",
+    significance:
+      "Many patterns on the clothing hold spiritual significance, representing reverence for deities.",
+  },
+  {
+    image: ede,
+    title: "E-de",
+    characteristic:
+      "The Ede, one of Vietnam’s ethnic minority groups, have a rich cultural heritage that is deeply tied to their traditional customs and way of life. One of the most distinctive aspects of Ede culture is their traditional costume, which reflects their connection to nature and their societal values.",
+    significance:
+      "The traditional costume of the Ede people holds deep cultural and spiritual significance, symbolizing their connection to nature, community, and ancestry. It is much more than just clothing; it represents the identity, social values, and traditions that have been passed down through generations.",
+  },
+  {
+    image: khomu,
+    title: "Kho Mu",
+    characteristic:
+      "The Nung people, another ethnic minority group in Vietnam, have a distinct culture with their own traditional customs, including clothing that carries deep cultural and spiritual significance.",
+    significance:
+      " The Nung’s traditional costume is characterized by its simplicity, elegance, and the symbolic meanings embedded in its designs and materials, reflecting their values, beliefs, and connection to nature and their community.",
+  },
+  {
+    image: nung,
+    title: "Nung",
+    characteristic:
+      "The Ede, one of Vietnam’s ethnic minority groups, have a rich cultural heritage that is deeply tied to their traditional customs and way of life. One of the most distinctive aspects of Ede culture is their traditional costume, which reflects their connection to nature and their societal values.",
+    significance:
+      "The traditional costume of the Ede people holds deep cultural and spiritual significance, symbolizing their connection to nature, community, and ancestry. It is much more than just clothing; it represents the identity, social values, and traditions that have been passed down through generations.",
+  },
+];
