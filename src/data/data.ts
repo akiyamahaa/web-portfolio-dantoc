68e9abf3efc96d4bd8980adfff815c7ddd560b38
Quang Akiyama <quanganhcf2@gmail.com>
2024-10-05 20:29:31 +0700

    finish project

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,38 +2,182 @@ import khmer from "../assets/image/khmer.jpg";
 import nung from "../assets/image/nung.jpg";
 import khomu from "../assets/image/khomu.jpg";
 import ede from "../assets/image/ede.jpg";
+import tay from "../assets/image/tay.jpg";
+import muong from "../assets/image/muong.jpg";
+import hmong from "../assets/image/hmong.jpg";
+import cham from "../assets/image/cham.jpg";
+import sandiu from "../assets/image/sandiu.jpg";
+import hoa from "../assets/image/hoa.jpg";
+import thai from "../assets/image/thai.jpg";
+import lao from "../assets/image/lao.jpg";
+import hanhi from "../assets/image/hanhi.jpg";
+import cotu from "../assets/image/cotu.jpg";
+import chut from "../assets/image/chut.jpg";
+import giay from "../assets/image/giay.jpg";
+import pathen from "../assets/image/pathen.jpeg";
+import dao from "../assets/image/dao.jpeg";
+import tho from "../assets/image/tho.jpg";
+import boy from "../assets/image/boy.jpg";
 
 export const arrayImg = [
   {
     image: khmer,
     title: "Khmer",
     characteristic:
-      "Khmer women typically wear long skirts, blouses, and sarongs. The long skirts often have multiple layers and are adorned with intricate patterns. Blouses feature round or V-necklines and can have short or long sleeves. Sarongs are wrapped around the waist, creating a graceful and elegant appearance. Khmer attire is also influenced by religion, particularly Buddhism.",
+      "Phụ nữ Khmer thường mặc váy dài, áo cánh, và quấn khăn sarong. Váy dài thường có nhiều lớp, được trang trí hoa văn cầu kỳ. Áo cánh có cổ tròn hoặc cổ chữ V, tay ngắn hoặc tay dài. Khăn sarong được quấn quanh eo, tạo nên vẻ uyển chuyển, duyên dáng. Trang phục Khmer cũng chịu ảnh hưởng của tôn giáo, đặc biệt là Phật giáo.",
     significance:
-      "Many patterns on the clothing hold spiritual significance, representing reverence for deities.",
-  },
-  {
-    image: ede,
-    title: "E-de",
-    characteristic:
-      "The Ede, one of Vietnam’s ethnic minority groups, have a rich cultural heritage that is deeply tied to their traditional customs and way of life. One of the most distinctive aspects of Ede culture is their traditional costume, which reflects their connection to nature and their societal values.",
-    significance:
-      "The traditional costume of the Ede people holds deep cultural and spiritual significance, symbolizing their connection to nature, community, and ancestry. It is much more than just clothing; it represents the identity, social values, and traditions that have been passed down through generations.",
-  },
-  {
-    image: khomu,
-    title: "Kho Mu",
-    characteristic:
-      "The Nung people, another ethnic minority group in Vietnam, have a distinct culture with their own traditional customs, including clothing that carries deep cultural and spiritual significance.",
-    significance:
-      " The Nung’s traditional costume is characterized by its simplicity, elegance, and the symbolic meanings embedded in its designs and materials, reflecting their values, beliefs, and connection to nature and their community.",
+      "Nhiều họa tiết trên trang phục có ý nghĩa tâm linh, thể hiện sự tôn kính đối với thần linh.",
   },
   {
     image: nung,
-    title: "Nung",
+    title: "Nùng",
     characteristic:
-      "The Ede, one of Vietnam’s ethnic minority groups, have a rich cultural heritage that is deeply tied to their traditional customs and way of life. One of the most distinctive aspects of Ede culture is their traditional costume, which reflects their connection to nature and their societal values.",
+      "Áo của người Nùng có ống tay rộng, phần cổ tay và cổ áo được trang trí bằng những mảnh vải sáng màu trên nền áo màu chàm. Đặc biệt những họa tiết thổ cẩm được khéo léo thêu trên áo gây ấn tượng cho người mặc.",
     significance:
-      "The traditional costume of the Ede people holds deep cultural and spiritual significance, symbolizing their connection to nature, community, and ancestry. It is much more than just clothing; it represents the identity, social values, and traditions that have been passed down through generations.",
+      "Nhiều họa tiết trên áo được lấy cảm hứng từ tự nhiên, như hoa, lá, chim, cá... Chúng không chỉ mang ý nghĩa thẩm mỹ mà còn thể hiện niềm tin vào thần linh, tự nhiên và cuộc sống.",
+  },
+  {
+    image: khomu,
+    title: "Khơ Mú",
+    characteristic:
+      "Trang phục đặc trưng của người Khơ Mú là áo cóm và váy. Áo cóm thường có màu tối, chất liệu dày dặn, giúp giữ ấm cơ thể trong điều kiện thời tiết khắc nghiệt. Váy của phụ nữ Khơ Mú thường có màu tối, độ dài ngắn ngang bắp chân, rất thuận tiện cho việc lao động.",
+    significance:
+      "Các họa tiết trên trang phục còn thể hiện những ước muốn về cuộc sống như tình yêu, hạnh phúc, sức khỏe, mùa màng bội thu…",
+  },
+  {
+    image: ede,
+    title: "Ê-đê",
+    characteristic:
+      "Trang phục đặc trưng của người Ê Đê là váy tấm và áo chui. Váy tấm thường được làm từ vải bông tự dệt, có độ bền cao và thấm hút mồ hôi tốt, có dáng dài, xòe rộng, phần eo được cột bằng dây hoặc thắt lưng. Trên váy thường được trang trí bằng các hoa văn sặc sỡ, được thêu tay tỉ mỉ. Áo chui được làm từ vải bông tự dệt, có độ dày vừa phải, cổ tròn, tay ngắn hoặc dài và không có cúc.",
+    significance:
+      "Những họa tiết trên trang phục thể hiện niềm tin vào thần linh như họa tiết con voi, mặt trăng, rồng,...",
+  },
+  {
+    image: tay,
+    title: "Tày",
+    characteristic:
+      "Trang phục đặc trưng của phụ nữ Tày gồm áo năm thân, quần đen, khăn mỏ quả. Áo được may từ năm tấm vải ghép lại, tạo nên dáng áo vừa vặn, thoải mái khi vận động. Quần ống rộng, có cạp cao, khăn được quấn đầu khéo léo. Trên áo trang trí bởi hoa văn đơn giản, chủ yếu là các đường kẻ sọc, hình học, thêu tay tỉ mỉ.",
+    significance:
+      "Việc sử dụng màu nhuộm tự nhiên và chất liệu vải tự dệt thể hiện sự tôn trọng thiên nhiên. Sự giản dị, tinh tế trong trang phục thể hiện sự khéo léo của người phụ nữ Tày.",
+  },
+  {
+    image: muong,
+    title: "Mường",
+    characteristic:
+      "Trang phục đặc trưng của phụ nữ Mường gồm áo cánh hoặc áo tứ thân, quần đen ống rộng và khăn đội đầu. Áo thường có cổ tròn, tay ngắn hoặc dài, được trang trí bằng các đường thêu, hoa văn tinh xảo. Quần đen ống rộng, cạp cao, tạo sự thoải mái khi vận động. Khăn thường được làm bằng vải bông, màu trắng hoặc màu chàm, trang trí bằng các đường thêu, hoa văn. Họa tiết trên trang phục thường là hoa, lá, chim, cá, hình học...",
+    significance:
+      "Các họa tiết trên trang phục thể hiện ý nghĩa tôn trọng vũ trụ, đất trời, con người của dân tộc Mường. Nó còn thể hiện sự khéo léo và tỉ mỉ của phụ nữ nơi đây.",
+  },
+  {
+    image: cham,
+    title: "Chăm",
+    characteristic:
+      "Trang phục truyền thống của phụ nữ Chăm gồm áo dài tay, váy quấn dài và khăn choàng đầu làm từ vải dệt tay với các hoa văn tinh xảo. Trang phục là sự kết hợp hài hòa từ những tua sợi đỏ, hạt cườm óng ánh và thắt lưng hoa văn giúp tôn nét duyên dáng. Khăn choàng đầu được làm từ vải mịn, thêu hoa văn đa dạng, viền bằng kim tuyến.",
+    significance:
+      "Trang phục thể hiện nét đẹp truyền thống và sự khéo léo trong nghệ thuật dệt vải của người Chăm, đồng thời thể hiện sự gắn bó với văn hóa và tín ngưỡng Hồi giáo.",
+  },
+  {
+    image: sandiu,
+    title: "Sán Díu",
+    characteristic:
+      "Trang phục truyền thống của phụ nữ Sán Díu gồm khăn đội đầu, áo yếm, áo dài xẻ tà hai bên, váy dài, thắt lưng và xà cạp. Áo yếm hình thoi, buộc lên cổ và eo, cổ áo đính khuy bạc, tua rua xanh đỏ. Khăn đội đầu màu đen, áo dài 4 thân, cổ bẻ có nẹp trơn, ve áo nẹp vải trắng hình chữ V, thắt eo bằng dải lụa xanh hoặc đỏ.",
+    significance:
+      "Trang phục thể hiện sự hòa hợp với thiên nhiên và văn hóa địa phương, giản dị nhưng tinh tế.",
+  },
+  {
+    image: hmong,
+    title: "H'Mông",
+    characteristic:
+      "Trang phục truyền thống của phụ nữ H'Mông gồm áo dài, váy qua đầu gối, và khăn đội đầu. Áo dài tay được trang trí bằng hoa văn thổ cẩm sặc sỡ. Váy xòe nhiều tầng với hoa văn nổi bật tạo cảm giác bồng bềnh. Thắt lưng kết hợp với áo và váy thường có màu đen hoặc hoa văn thổ cẩm.",
+    significance:
+      "Trang phục phản ánh sự khéo léo trong nghệ thuật thêu tay, tình yêu thiên nhiên và là biểu tượng cho sự kiên cường, độc lập của dân tộc H'Mông.",
+  },
+  {
+    image: hoa,
+    title: "Hoa",
+    characteristic:
+      "Trang phục truyền thống của phụ nữ Hoa thường là áo cổ viền cao, cài khuy một bên, xẻ tà cao hoặc áo 'sườn xám' dài, ôm ngang hông, xẻ tà dưới phần đùi. Màu sắc trang phục thường đậm, đặc biệt là hồng hoặc đỏ. Trang phục thường thêu hoặc in họa tiết tinh xảo như rồng, phượng, hoa sen, mây...",
+    significance:
+      "Trang phục của người Hoa phản ánh sự phong phú về văn hóa và thể hiện lòng kính trọng đối với tổ tiên, truyền thống lâu đời.",
+  },
+  {
+    image: thai,
+    title: "Thái",
+    characteristic:
+      "Phụ nữ Thái thường mặc áo cánh ngắn, váy dài màu đen có họa tiết sặc sỡ ở gấu váy, kết hợp với khăn piêu và dây lưng nhiều màu. Áo có cổ tròn, tay ngắn hoặc dài, trang trí bằng các đường thêu hoa văn đơn giản. Váy đen dài đến mắt cá, may từ vải bông tự dệt, thoải mái và mát mẻ.",
+    significance:
+      "Trang phục thể hiện sự duyên dáng, lịch lãm của người Thái, là biểu tượng của sự gắn kết cộng đồng và tôn trọng thiên nhiên.",
+  },
+  {
+    image: hanhi,
+    title: "Hà Nhì",
+    characteristic:
+      "Phụ nữ Hà Nhì mặc áo dài, váy dài và khăn đội đầu với màu chủ đạo xanh và đen. Áo làm từ vải bông tự dệt, cổ áo và nẹp áo được trang trí bằng những đường thêu tỉ mỉ với họa tiết hình học, động vật như chim, cá, hoa lá. Váy dài đến mắt cá, thường có màu đen hoặc xanh đậm.",
+    significance:
+      "Trang phục phản ánh sự cầu kỳ trong nghệ thuật thêu và biểu hiện sự gắn bó với văn hóa truyền thống, cuộc sống thiên nhiên.",
+  },
+  {
+    image: lao,
+    title: "Lào",
+    characteristic:
+      "Phụ nữ Lào mặc váy quấn (sinh) và áo dài tay, kết hợp với khăn choàng vai. Váy quấn làm từ cotton hoặc lụa, được trang trí bằng họa tiết thêu tay tinh xảo. Áo dài tay có thể là áo cánh hoặc cổ tròn, màu trơn hoặc họa tiết đơn giản, hài hòa với sinh. Khăn choàng vai là phụ kiện không thể thiếu, giúp giữ ấm và tăng thêm vẻ đẹp.",
+    significance:
+      "Trang phục của người Lào thể hiện sự duyên dáng và gắn bó với văn hóa, tín ngưỡng Phật giáo.",
+  },
+  {
+    image: cotu,
+    title: "Cơ Tu",
+    characteristic:
+      "Phụ nữ Cơ Tu mặc váy quấn dài, áo cánh ngắn và khăn quấn đầu. Váy quấn được dệt bằng vải bông tự nhiên, có độ bền cao và thấm hút tốt, với hoa văn đa dạng thêu tay tinh xảo. Màu sắc chủ đạo là chàm đen, nâu đất hoặc trắng. Áo cánh ngắn thường cổ tròn hoặc vuông, tạo sự thoải mái khi vận động.",
+    significance:
+      "Trang phục của người Cơ Tu thể hiện sự gắn kết với thiên nhiên, cuộc sống du mục và biểu tượng cho sức mạnh, lòng dũng cảm.",
+  },
+  {
+    image: pathen,
+    title: "Pà Thẻn",
+    characteristic:
+      "Phụ nữ Pà Thẻn mặc váy dài, áo chít eo, trang trí bằng họa tiết thêu tay phức tạp. Váy dài đến mắt cá chân, có xẻ tà để di chuyển dễ dàng. Áo chít eo ôm sát cơ thể, phần tay áo thường ngắn hoặc dài đến khuỷu tay.",
+    significance:
+      "Trang phục của người Pà Thẻn phản ánh nét văn hóa riêng biệt, thể hiện niềm tự hào dân tộc và sự khéo léo trong nghệ thuật thêu.",
+  },
+  {
+    image: giay,
+    title: "Giáy",
+    characteristic:
+      "Phụ nữ Giáy thường mặc áo dài tay, váy dài và khăn quấn đầu. Áo màu xanh nước biển, đơn giản nhưng tinh tế, được trang trí bằng các đường kẻ sọc hoặc ô vuông. Váy dài ôm sát cơ thể, hài hòa với áo, khăn quấn đầu có họa tiết ô vuông.",
+    significance:
+      "Trang phục của người Giáy thể hiện sự đơn giản, mộc mạc, nhưng tinh tế, phản ánh cuộc sống gắn liền với thiên nhiên và lao động sản xuất.",
+  },
+  {
+    image: chut,
+    title: "Chứt",
+    characteristic:
+      "Trang phục người Chứt đơn giản với áo và váy từ vải thô. Áo cánh ngắn, cổ tròn hoặc vuông, váy dài quấn quanh eo. Trước đây, người Chứt sử dụng vỏ cây rừng như sui, sàng, si... để làm trang phục, nay dùng vải thô từ sợi bông hoặc lanh.",
+    significance:
+      "Trang phục của người Chứt thể hiện sự giản dị, gần gũi với thiên nhiên và phản ánh cuộc sống tự cung tự cấp.",
+  },
+  {
+    image: boy,
+    title: "Bồ Y",
+    characteristic:
+      "Phụ nữ Bồ Y mặc áo dài tay và váy dài, màu đen hoặc xanh lam, với họa tiết thêu tay đơn giản. Áo dài có cổ tròn hoặc cổ vuông, may vừa vặn cơ thể, tạo sự thoải mái khi vận động. Váy dài đến mắt cá chân, với họa tiết hài hòa cùng áo, tạo nên sự uyển chuyển.",
+    significance:
+      "Trang phục của người Bồ Y thể hiện sự tôn trọng và gắn bó với truyền thống văn hóa, biểu tượng cho sự giản dị và mộc mạc.",
+  },
+  {
+    image: tho,
+    title: "Thổ",
+    characteristic:
+      "Phụ nữ Thổ mặc áo cánh ngắn, váy dài và khăn quấn đầu với họa tiết thổ cẩm đa dạng. Áo ngắn ôm sát cơ thể, tay áo dài đến khuỷu tay. Váy dài đến mắt cá chân, phần chân váy xòe nhẹ. Khăn quấn đầu làm từ vải bông hoặc lụa, nhiều màu sắc và họa tiết khác nhau.",
+    significance:
+      "Trang phục của người Thổ thể hiện sự phong phú và đa dạng văn hóa, là biểu tượng cho sự khéo léo trong nghề dệt thổ cẩm.",
+  },
+  {
+    image: dao,
+    title: "Dao",
+    characteristic:
+      "Phụ nữ Dao nổi bật với bộ trang phục đỏ, gồm áo dài thêu hoa văn tinh xảo, váy dài và khăn đội đầu thêu hoa văn và hạt cườm. Áo dài ôm sát cơ thể, với hoa văn lấy cảm hứng từ thiên nhiên và tín ngưỡng, thêu bằng chỉ đỏ, vàng, xanh. Váy và khăn cũng được thêu hoa văn hài hòa.",
+    significance:
+      "Trang phục của người Dao phản ánh sự tôn vinh tổ tiên, gắn bó với thiên nhiên và tín ngưỡng dân gian, biểu tượng cho sự kiên cường và độc lập.",
   },
 ];
